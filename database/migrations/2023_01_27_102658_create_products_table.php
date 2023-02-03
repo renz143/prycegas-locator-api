70187ee5f8990aa72306be10fa308c042418b057
Renz <@whilcerenzmacaambac@gmail.com>
2023-02-03 09:18:57 +0800

    rename products typo

--- database/migrations/2023_01_27_102658_create_products_table.php
+++ database/migrations/2023_01_27_102658_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products_', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('pricebook_id');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products_');
+        Schema::dropIfExists('products');
     }
 };
